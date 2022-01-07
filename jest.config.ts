1eba403f31d1d91c72d45889fc03da12e148d15d
David <davie668@gmail.com>
2022-01-07 13:31:52 +0000

    Init

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  clearMocks: true,
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  transform: {}
+}
+
+export default config
